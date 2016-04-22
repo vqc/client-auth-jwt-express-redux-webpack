4380dc9c36452946c1aaceb0d007deaf1ace71ac
Vince <vince@cvq.cc>
2016-04-22 00:22:08 -0700

    add error handling for login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
-import { AUTH_USER } from './types';
+import {
+  AUTH_USER,
+  AUTH_ERROR
+} from './types';
 
 const ROOT_URL = 'http://localhost:4040';
 
@@ -33,7 +36,14 @@ export function signinUser({email, password}){
         browserHistory.push('/feature');
       })
       .catch( () => {
-
+        dispatch(authError('Bad Login Info'));
       });
+  }
+}
+
+export function authError(error){
+  return {
+    type: AUTH_ERROR,
+    payload: error
   }
 }
--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -12,6 +12,7 @@ class Signin extends Component{
   constructor(props){
     super(props);
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.renderAlert = this.renderAlert.bind(this);
   }
   render(){
     //handleSubmit and email and password all come from redux form
@@ -31,8 +32,9 @@ class Signin extends Component{
         </fieldset>
         <fieldset className='form-group'>
           <label>Password:</label>
-          <input {...password} className="form-control" />
+          <input {...password} type="password" className="form-control" />
         </fieldset>
+        {this.renderAlert()}
         <button action="submit" className="btn btn-primary">Sign In</button>
       </form>
     );
@@ -41,10 +43,25 @@ class Signin extends Component{
     console.log(email, password);
     return this.props.signinUser({email, password});
   }
+  renderAlert(){
+    if (this.props.errorMessage){
+      return(
+        <div className="alert alert-danger">
+          <strong>Oops!</strong> {this.props.errorMessage}
+        </div>
+      )
+    }
+  }
+}
+
+function mapStateToProps(state){
+  return {
+    errorMessage: state.auth.error
+  };
 }
 
 //first parans is for config, second parans is for our react component
 export default reduxForm({
   form:'signin', //name in the state
   fields: ['email', 'password']
-}, null, actions)(Signin);
+}, mapStateToProps, actions)(Signin);
