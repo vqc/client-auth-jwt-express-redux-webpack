e68b70c12d1a60624e7d7abdcd855a6f9d56e446
Vince <vince@cvq.cc>
2016-04-21 23:51:54 -0700

    add action and reducer for programmatic routing and setting auth state using redux thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER } from './types';
 
 const ROOT_URL = 'http://localhost:4040';
 
@@ -18,6 +20,18 @@ export function signinUser({email, password}){
 
     //submit to credentials to api server
     //first is the url, second is the object containing the body
-    axios.post(`${ROOT_URL}/signin`, { email, password });
+    //axios always returns a promise
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then(response => { //success case
+        //update state to indicate user is authenticated
+        dispatch({
+          type: AUTH_USER,
+        })
+        //redirect to the route /feature
+        browserHistory.push('/feature');
+      })
+      .catch( () => {
+
+      });
   }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-
 //redeclare reducer variable as form
 //so that we dont have to have a reducer called reducer
 import { reducer as form } from 'redux-form';
+import authReducer from './auth_reducer';
 
 const rootReducer = combineReducers({
   /**
@@ -15,7 +15,8 @@ const rootReducer = combineReducers({
       error: string //display an error related to authentication has occured
     }
   **/
-  form: form //can replace this with just 'form' because of es6
+  form: form, //can replace this with just 'form' because of es6
+  auth: authReducer
 });
 
 export default rootReducer;
