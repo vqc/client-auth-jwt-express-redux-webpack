b7d2968a7329223bb4d05653239a69ced74c8020
Vince <vince@cvq.cc>
2016-04-22 18:36:51 -0700

    connect header to authentication state

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,48 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 class Header extends Component {
+  constructor(props){
+    super(props)
+    this.renderLinks = this.renderLinks.bind(this);
+  }
   render(){
     return (
       <nav className="navbar navbar-light">
+        <Link to="/" className="navbar-brand">Brighten</Link>
         <ul className="nav navbar-nav">
-          <li className="nav-item">
-            Sign In
-          </li>
+          {this.renderLinks()}
         </ul>
       </nav>
     );
   }
+  renderLinks(){
+    if(this.props.authenticated){
+      return (
+        <li className="nav-item">
+          <Link className='nav-link' to='/signout'>Sign Out</Link>
+        </li>
+      )
+    }else{
+      return (
+        [
+          <li className="nav-item" key="signin">
+            <Link className="nav-link" to ="/signin">Sign In</Link>
+          </li>,
+          <li className="nav-item" key="signup">
+            <Link className="nav-link" to ="/signup">Sign Up</Link>
+          </li>
+        ]
+      )
+    }
+  }
 }
 
-export default Header;
+function mapStateToProps(state){
+  return {
+    authenticated: state.auth.authenticated,
+  }
+}
+
+export default connect(mapStateToProps)(Header);
